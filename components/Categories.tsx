561fa52ed2d5ebc7a0655b847e1cbb4464edd089
SoundtrkSamurai <furtimat3@yahoo.com>
2024-07-01 21:12:44 -0700

    home-screen: animate and give type to Category

--- components/Categories.tsx
+++ components/Categories.tsx
@@ -1,50 +1,62 @@
 import { Pressable, ScrollView, StyleSheet, Text, View } from "react-native";
 import React, { Dispatch, SetStateAction } from "react";
-import { categoryData } from "@/constants/index";
 import { Image } from "expo-image";
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
 } from "react-native-responsive-screen";
+import Animated, { FadeInDown } from "react-native-reanimated";
+
+export type Category = {
+  strCategory: string;
+  strCategoryThumb: string;
+  idCategory: string;
+  strCategoryDescription: string;
+};
 
 type Props = {
   activeCategory: string;
+  categories: Category[];
   setActiveCategory: Dispatch<SetStateAction<string>>;
 };
 
-const Categories = ({ activeCategory, setActiveCategory }: Props) => {
+const Categories = ({
+  activeCategory,
+  categories,
+  setActiveCategory,
+}: Props) => {
   return (
-    <View style={styles.container}>
+    <Animated.View entering={FadeInDown.duration(500).springify()}>
       <ScrollView
         horizontal
         showsVerticalScrollIndicator={false}
         className="space-x-4"
         contentContainerStyle={{ paddingHorizontal: 15 }}
       >
-        {categoryData.map((cat, index) => {
-          let isActive = cat.name == activeCategory;
+        {categories.map((cat, index) => {
+          let isActive = cat.strCategory == activeCategory;
           let activeButtonClass = isActive ? "bg-amber-400" : "bg-black/10";
           return (
             <Pressable
               key={index}
-              className="flex items-center space-y-1"
-              onPress={() => setActiveCategory(cat.name)}
+              className="flex items-center mx-2.5 space-y-1"
+              onPress={() => setActiveCategory(cat.strCategory)}
             >
               <View className={`p-4 rounded-full ${activeButtonClass}`}>
                 <Image
-                  source={{ uri: cat.image }}
+                  source={{ uri: cat.strCategoryThumb }}
                   style={{ width: hp(6), height: hp(6) }}
                   className="rounded-full"
                 />
               </View>
               <Text className="text-neutral-600" style={{ fontSize: hp(1.6) }}>
-                {cat.name}
+                {cat.strCategory}
               </Text>
             </Pressable>
           );
         })}
       </ScrollView>
-    </View>
+    </Animated.View>
   );
 };
 
