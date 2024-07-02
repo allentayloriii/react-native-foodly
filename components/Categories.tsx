7fe06c60ea02fd616551b85f5e441ab9788c4321
SoundtrkSamurai <furtimat3@yahoo.com>
2024-07-01 20:08:18 -0700

    home-screen: create Categories component

--- components/Categories.tsx
+++ components/Categories.tsx
@@ -1,7 +1,18 @@
-import { ScrollView, StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { Pressable, ScrollView, StyleSheet, Text, View } from "react-native";
+import React, { Dispatch, SetStateAction } from "react";
+import { categoryData } from "@/constants/index";
+import { Image } from "expo-image";
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
 
-const Categories = () => {
+type Props = {
+  activeCategory: string;
+  setActiveCategory: Dispatch<SetStateAction<string>>;
+};
+
+const Categories = ({ activeCategory, setActiveCategory }: Props) => {
   return (
     <View style={styles.container}>
       <ScrollView
@@ -10,7 +21,28 @@ const Categories = () => {
         className="space-x-4"
         contentContainerStyle={{ paddingHorizontal: 15 }}
       >
-        <Text>Categories</Text>
+        {categoryData.map((cat, index) => {
+          let isActive = cat.name == activeCategory;
+          let activeButtonClass = isActive ? "bg-amber-400" : "bg-black/10";
+          return (
+            <Pressable
+              key={index}
+              className="flex items-center space-y-1"
+              onPress={() => setActiveCategory(cat.name)}
+            >
+              <View className={`p-4 rounded-full ${activeButtonClass}`}>
+                <Image
+                  source={{ uri: cat.image }}
+                  style={{ width: hp(6), height: hp(6) }}
+                  className="rounded-full"
+                />
+              </View>
+              <Text className="text-neutral-600" style={{ fontSize: hp(1.6) }}>
+                {cat.name}
+              </Text>
+            </Pressable>
+          );
+        })}
       </ScrollView>
     </View>
   );
