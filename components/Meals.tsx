35dc43735c24925ad74374abfaa5dce2eb73ef86
SoundtrkSamurai <furtimat3@yahoo.com>
2024-07-04 14:50:18 -0700

    home-screen: Create Meals component

---
+++ components/Meals.tsx
@@ -0,0 +1,85 @@
+import { mealData } from "@/constants/index";
+import MasonryList from "@react-native-seoul/masonry-list";
+import { Image } from "expo-image";
+import React from "react";
+import { Pressable, Text, View } from "react-native";
+import Animated, { FadeInDown } from "react-native-reanimated";
+import { heightPercentageToDP as hp } from "react-native-responsive-screen";
+import { Category } from "./Categories";
+
+export type Meal = {
+  idMeal: string;
+  strMeal: string;
+  strMealThumb: string;
+};
+type Props = {
+  categories: Category[];
+  meals: Meal[];
+};
+
+const Meals = ({ categories, meals }: Props) => {
+  return (
+    <View className="mx-4 space-y-3">
+      <Text
+        className="font-semibold text-neutral-600"
+        style={{ fontSize: hp(3) }}
+      >
+        Meals
+      </Text>
+      <View>
+        {categories.length > 0 && (
+          <MasonryList
+            data={mealData}
+            keyExtractor={(item): string => item.id}
+            numColumns={2}
+            showsVerticalScrollIndicator={false}
+            renderItem={({ item, i }) => <MealCard item={item} index={i} />}
+            // refreshing={isLoadingNext}
+            // onRefresh={() => refetch({ first: ITEM_CNT })}
+            onEndReachedThreshold={0.1}
+            // onEndReached={() => loadNext(ITEM_CNT)}
+          />
+        )}
+      </View>
+    </View>
+  );
+};
+
+const MealCard = ({ item, index }) => {
+  let isEven = index % 2 === 0;
+  return (
+    <Animated.View
+      entering={FadeInDown.delay(index * 100)
+        .duration(600)
+        .springify()
+        .damping(12)}
+    >
+      <Pressable
+        style={{
+          width: "100%",
+          paddingLeft: isEven ? 0 : 8,
+          paddingRight: isEven ? 8 : 0,
+        }}
+        className="flex justify-center mb-4 space-y-1"
+      >
+        <Image
+          source={{ uri: item.image }}
+          style={{
+            width: "100%",
+            height: index % 3 === 0 ? hp(25) : hp(35),
+            borderRadius: 35,
+          }}
+          className="bg-black/5"
+        />
+        <Text
+          className="ml-2 font-semibold text-neutral-600"
+          style={{ fontSize: hp(1.5) }}
+        >
+          {item.name.length > 20 ? item.name.slice(0, 20) + "..." : item.name}
+        </Text>
+      </Pressable>
+    </Animated.View>
+  );
+};
+
+export default Meals;
